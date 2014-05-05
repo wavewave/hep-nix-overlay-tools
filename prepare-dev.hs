cf177dcef7bd81722b2e6c58567072a1fd02dc01
Ian-Woo Kim <ianwookim@gmail.com>
2014-05-05 15:50:22 +0000

    use cmdargs. now make symbolic links from pkg output and from env output

--- prepare-dev.hs
+++ prepare-dev.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
 
 import Control.Applicative
@@ -5,12 +7,29 @@ import Control.Monad
 -- import Control.Monad.Trans.Either
 -- import qualified Data.Foldable as F
 import Data.List
+import System.Console.CmdArgs
 import System.Directory
 import System.Environment
 import System.Exit
 import System.FilePath
 import System.IO.Error
+import System.Posix.Files
 import System.Process
+
+data HepNixOverlayTools = HepNixOverlayTools { pkgname :: String
+                                             , envname :: String
+                                             , resultPkgPath :: FilePath
+                                             , resultEnvPath :: FilePath
+                                             }
+                        deriving (Show, Data, Typeable)  
+
+tools :: HepNixOverlayTools
+tools = HepNixOverlayTools { pkgname = def &= argPos 0 &= typ "MAINPKG"
+                           , envname = def &= argPos 1 &= typ "ENVPKG"
+                           , resultPkgPath = "resultpkg" &= opt "resultpkg"
+                           , resultEnvPath = "resultenv" &= opt "resultenv"
+                           } 
+ 
 
 
 simpleRunCmd :: String -> [String] -> IO (ExitCode,String,String)
@@ -43,14 +62,6 @@ getReferencesFromDeriv deriv = do
   res <- nixStore ["--query", "--references", deriv] 
   (return . either Left (Right . lines . fst) . excodeToEither) res
 
-{-
-getReferencesFromOneAttrib :: String -> IO [FilePath]
-getReferencesFromOneAttrib aname = do 
-  Right (sout,serr) <- mkDerivationFromHepNixOverlayAttrib aname
-  let act x = do Right lst <- getReferencesFromDeriv x
-                 return lst
-  concat <$> mapM act (lines sout)
--}
 
 obtainFromAttrib :: (FilePath -> IO (Either String [String])) -> String -> IO [String]
 obtainFromAttrib f aname = do 
@@ -66,33 +77,54 @@ getOutputsFromDeriv deriv = do
   (return . either Left (Right . lines . fst) . excodeToEither) res
 
 
-  
+mkSymbLnks :: FilePath -> [FilePath] -> IO ()
+mkSymbLnks fp [] = return ()
+mkSymbLnks fp (x:[]) = createSymbolicLink x fp 
+mkSymbLnks fp xs = 
+  let ys = zip [1..] xs 
+  in mapM_ (\(f,s)->createSymbolicLink (fp++show f) s) ys
+
 
 main :: IO ()
 main = do
-  args <- getArgs 
-  let names = [args !! 0] 
-      envname = args !! 1
- 
-  drvlst <- concat <$> mapM (obtainFromAttrib (return . Right . lines)) names 
+  opts <- cmdArgs tools
+  let pkg = pkgname opts
+      env = envname opts
+      resultpkg = resultPkgPath opts
+      resultenv = resultEnvPath opts
+
+  -- print (pkg,env)
+  -- print (resultpkg,resultenv)
+  
+  drvlst <- concat <$> mapM (obtainFromAttrib (return . Right . lines)) [pkg]
   putStrLn "drvs = " 
   mapM_ putStrLn drvlst
   putStrLn "************"
-  lst <- concat <$> mapM (obtainFromAttrib getOutputsFromDeriv)  names
-  let olst = (nub . sort) lst
-  putStrLn "outputs=" 
-  mapM_ putStrLn olst 
+
+
+  pkgolst <- (nub . sort . concat) <$> mapM (obtainFromAttrib getOutputsFromDeriv) [pkg]
+  putStrLn "making links of output path from main package derivation" 
+  mapM_ putStrLn pkgolst 
+  mkSymbLnks resultpkg pkgolst
+
+  envolst <- (nub . sort) <$> obtainFromAttrib getOutputsFromDeriv env
+  putStrLn "making the link of output path from env package derivation" 
+  mapM_ putStrLn envolst 
+  mkSymbLnks resultenv envolst
+ 
+
+  {- 
   putStrLn "========"
   putStrLn "references="
-  lst' <- concat <$> mapM (obtainFromAttrib getReferencesFromDeriv)  names
+  lst' <- concat <$> mapM (obtainFromAttrib getReferencesFromDeriv) [pkg]
   let rlst' = (nub . sort) lst'
   mapM_ putStrLn rlst'
 
   putStrLn "===env ref before======="
-  erlst <- obtainFromAttrib getReferencesFromDeriv envname
+  erlst <- obtainFromAttrib getReferencesFromDeriv env
   mapM_ putStrLn erlst
   putStrLn "===env ref after=======" 
   let erlst' = filter (not . flip elem drvlst) erlst
   mapM_ putStrLn erlst'
- 
+  -}
   
